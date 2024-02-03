ac3772229b45b7d84b9a3399fb70bb2cbbbb7d30
lambhatenavnath55 <lambhatenavnath55@gmail.com>
2024-02-03 18:40:01 +0530

    adding dynamic content

--- college_project/college_app/views.py
+++ college_project/college_app/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render
 from .models import College
 from .serializers import CollegeSerializer
+import datetime
+import random
 
 # Create your views here.
 from django.shortcuts import render
 
 def dashboard(request):
     template_name = 'college_app/dashboard.html'
-    return render(request, template_name)
+    current_time = datetime.datetime.now()
+    random_quote = get_random_quote()
+    context = {
+        'current_time': current_time,
+        'random_quote': random_quote,
+    }
+    return render(request, template_name, context)
+
+def get_random_quote():
+    quotes = ["Teachers can open the door, but you must enter it yourself. — Chinese proverb",
+        "Education is the passport to the future, for tomorrow belongs to those who prepare for it today. — Malcolm X",
+        "Education is the key to unlock the golden door of freedom."
+    ]
+    return random.choice(quotes)
 
 def home(request):
     template_name = 'college_app/home.html'
