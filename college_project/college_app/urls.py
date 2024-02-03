3a4c7ac2002ec800edbdf9e553b39a0c184d3b22
lambhatenavnath55 <lambhatenavnath55@gmail.com>
2024-02-03 18:10:06 +0530

    creating python web server

--- college_project/college_app/urls.py
+++ college_project/college_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+    path('home/', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact')
 ]
--- college_project/college_app/views.py
+++ college_project/college_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import College
+from .serializers import CollegeSerializer
 
 # Create your views here.
 from django.shortcuts import render
@@ -6,3 +8,19 @@ from django.shortcuts import render
 def dashboard(request):
     template_name = 'college_app/dashboard.html'
     return render(request, template_name)
+
+def home(request):
+    template_name = 'college_app/home.html'
+    clg_info = College.objects.all()
+    serializer =  CollegeSerializer(clg_info, many=True)
+    return render(request, template_name, {'serialized_data': serializer.data})
+
+def about(request):
+    template_name = 'college_app/about.html'
+    college_object = College.objects.first()
+    return render(request, template_name, {'description': college_object.description})
+
+def contact(request):
+    template_name = 'college_app/contact.html'
+    college_object = College.objects.first()
+    return render(request, template_name, {'contact':college_object.contact_email})
